c8a21c8f662a6c68f42ad9b097b5f33dbb54982c
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-17 16:53:09 +0000

    change script

---
+++ check_eks_cluster.sh
@@ -0,0 +1,16 @@
+
+#check if cluster exists on aws
+aws eks describe-cluster --name old-cluster
+
+if [ $? -eq 0 ]; then
+    echo "Cluster Exists!"
+else
+#creates new cluster
+    aws eks create-cluster \
+   --region us-east-1 \
+   --name old-cluster \
+   --kubernetes-version 1.20 \
+   --role-arn arn:aws:iam::852515165118:role/Eks-Cluster_role \
+   --resources-vpc-config subnetIds=subnet-00602b66,subnet-7c44155d,subnet-a5dd89fa,subnet-f06d77bd,subnet-19fff217,securityGroupIds=sg-d266f7c9
+    echo "New Cluster Created"
+fi
