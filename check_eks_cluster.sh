02d7ec1ed9f7452246ee460d33ea2981c6ab349c
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-18 15:24:31 +0000

    try new cluster with endpoints

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -1,6 +1,6 @@
 
 #check if cluster exists on aws
-aws eks describe-cluster --name old-cluster
+aws eks describe-cluster --name old-cluster 
 
 if [ $? -eq 0 ]; then
     echo "Cluster Exists!"
@@ -9,8 +9,8 @@ else
     aws eks create-cluster \
    --region us-east-1 \
    --name old-cluster \
-   --kubernetes-version 1.20 \
+   --kubernetes-version 1.21 \
    --role-arn arn:aws:iam::852515165118:role/Eks-Cluster_role \
-   --resources-vpc-config subnetIds=subnet-00602b66,subnet-7c44155d,subnet-a5dd89fa,subnet-f06d77bd,subnet-19fff217,securityGroupIds=sg-d266f7c9
+   --resources-vpc-config subnetIds=subnet-00602b66,subnet-7c44155d,subnet-a5dd89fa,subnet-f06d77bd,subnet-19fff217,securityGroupIds=sg-d266f7c9,endpointPublicAccess=true,endpointPrivateAccess=true
     echo "New Cluster Created"
 fi
