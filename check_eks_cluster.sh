87d461d051e3d3c41f3baba1c6c2281b526fd1cd
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-19 15:02:51 +0000

    test create cluster

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -1,16 +1,21 @@
 
 #check if cluster exists on aws
-aws eks describe-cluster --name old-cluster 
+aws eks describe-cluster --name capstone-cluster 
 
 if [ $? -eq 0 ]; then
     echo "Cluster Exists!"
 else
 #creates new cluster
-    aws eks create-cluster \
-   --region us-east-1 \
-   --name old-cluster \
-   --kubernetes-version 1.21 \
-   --role-arn arn:aws:iam::852515165118:role/Eks-Cluster_role \
-   --resources-vpc-config subnetIds=subnet-00602b66,subnet-7c44155d,subnet-a5dd89fa,subnet-f06d77bd,subnet-19fff217,securityGroupIds=sg-d266f7c9,endpointPublicAccess=true,endpointPrivateAccess=true
+    eksctl create cluster \
+    --name test-cluster \
+    --region us-east-1 \
+    --name react-app \
+    --node-type t2.large \
+    --nodes 3 \
+    --nodes-min 2 \
+    --nodes-max 4 \
+    --ssh-access \
+    --ssh-public-key AWS_EC2_DEMO \
+    --managed
     echo "New Cluster Created"
 fi
