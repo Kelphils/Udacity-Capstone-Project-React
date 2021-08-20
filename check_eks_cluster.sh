b6dbb626faaec880811aa619ed82970ef1b03f23
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-20 19:39:07 +0000

    update check-cluster 5

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -1,5 +1,5 @@
 #install kubectl
-curl -LO https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl.sha256
+curl -LO https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl
 
 chmod +x kubectl
 kubectl version --client
