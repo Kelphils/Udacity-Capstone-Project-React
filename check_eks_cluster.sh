02d3203600414a9b68f19ed311ee8ee15eaeb247
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-20 11:00:52 +0000

    try new config 85

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -1,6 +1,3 @@
-
-
-
 #install kubectl
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
 
@@ -8,16 +5,22 @@ chmod +x ./kubectl
 
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-#check if cluster exists on aws
+
+#install eksctl
 yum install -y tar gzip
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
 
 eksctl version
+#check if cluster exists on aws
+aws eks describe-cluster --name capstone-cluster 
+if [ $? -eq 0 ]; then
+    echo "Cluster Exists!"
+else
 #creates new cluster
     eksctl create cluster \
-    --name test-cluster \
+    --name capstone-cluster \
     --region us-east-1 \
     --name react-app \
     --node-type t2.large \
@@ -28,3 +31,4 @@ eksctl version
     --ssh-public-key AWS_EC2_DEMO \
     --managed
     echo "New Cluster Created"
+fi
