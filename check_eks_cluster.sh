bcdf49d8d00f3d2919f864ea30bc1ecdb4d22f35
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-20 11:05:00 +0000

    try new config 85

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -18,17 +18,16 @@ aws eks describe-cluster --name capstone-cluster
 if [ $? -eq 0 ]; then
     echo "Cluster Exists!"
 else
-#creates new cluster
     eksctl create cluster \
-    --name capstone-cluster \
-    --region us-east-1 \
-    --name react-app \
-    --node-type t2.large \
-    --nodes 3 \
-    --nodes-min 2 \
-    --nodes-max 4 \
-    --ssh-access \
-    --ssh-public-key AWS_EC2_DEMO \
-    --managed
+        --name capstone-cluster \
+        --region us-east-1 \
+        --name react-app \
+        --node-type t2.large \
+        --nodes 3 \
+        --nodes-min 2 \
+        --nodes-max 4 \
+        --ssh-access \
+        --ssh-public-key AWS_EC2_DEMO \
+        --managed
     echo "New Cluster Created"
 fi
