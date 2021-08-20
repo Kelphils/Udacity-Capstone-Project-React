c745ddf56a8e568febfff00159a5215c505ec492
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-20 08:07:49 +0000

    install kubectl

---
+++ install_kubectl.sh
@@ -0,0 +1,7 @@
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
