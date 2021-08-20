f3c17c36dfbafe73cce920aa9adbb5e25b9977d8
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-20 19:34:32 +0000

    update check-cluster 3

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -1,10 +1,10 @@
 #install kubectl
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl.sha256
 
-chmod +x ./kubectl
-
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+chmod +x kubectl
+mkdir -p ~/.local/bin/kubectl
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version --client
 
 #install eksctl
 yum install -y tar gzip
