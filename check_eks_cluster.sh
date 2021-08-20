609d577f518612e04b72f5a84ac03f741d5c3de5
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-20 10:05:17 +0000

    try new config 4

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -2,8 +2,7 @@
 
 
 #install kubectl
-
-wget curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
 
 chmod +x ./kubectl
 
