c37192b35d41f999e347f364fbf8a1d278b5cd52
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-20 10:48:26 +0000

    try new config 81

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -12,7 +12,7 @@ echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 yum install -y tar gzip
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
+mv /tmp/eksctl /usr/local/bin
 
 eksctl version
 #creates new cluster
