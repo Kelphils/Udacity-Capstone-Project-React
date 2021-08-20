85e9bb6a54e44ac4fd767d81941841858e73a71b
EC2 Default User <ec2-user@ip-172-31-11-225.ec2.internal>
2021-08-20 10:47:03 +0000

    try new config 80

--- check_eks_cluster.sh
+++ check_eks_cluster.sh
@@ -10,6 +10,7 @@ mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 #check if cluster exists on aws
 yum install -y tar gzip
+
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
